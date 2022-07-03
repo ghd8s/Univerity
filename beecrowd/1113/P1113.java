8a28ec14cc9edb899e6390baa9023a87ed1ce62e
Gustavo-Henrique <h.gustavospeed2002@gmail.com>
2022-07-03 11:56:24 -0300

    1113

--- beecrowd/1113/P1113.java
+++ beecrowd/1113/P1113.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class P1113{
 	public static void main(String[] args){
 		var s = new Scanner(System.in);
-		int x = 0;
-		int y = 0;
+		int x, y;
 		do {
 			x = s.nextInt();
 			y = s.nextInt();
 			if (x<y) System.out.println("Crescente");
 			else if (y<x) System.out.println("Decrescente");
 		} while(x != y);
+		s.close();
 	}
 }
